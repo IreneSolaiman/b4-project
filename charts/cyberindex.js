b83c3fca70748abf07050602dab9deebcebe4d4a
WEE KEONG ANG <wea871@student.hks.harvard.edu>
2018-01-14 20:13:37 -0500

    add third chart on africa

--- charts/cyberindex.js
+++ charts/cyberindex.js
@@ -1,95 +1,106 @@
-d3.csv('charts/cyberindex.csv', function (data) {
-  // Variables
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function drawChart(css_selector, dataset, dotcolor){
 
-var x = d3.scale.linear()
-    .range([0, width]);
+  d3.csv(dataset, function (data) {
+    // Variables
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-var y = d3.scale.linear()
-    .range([height, 0])
-    .domain([0, 1]);
+  var x = d3.scale.linear()
+      .range([0, width])
+      .domain([0, 100]);
 
-var color = d3.scale.category10();
+  var y = d3.scale.linear()
+      .range([height, 0])
+      .domain([0, 1]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  var color = d3.scale.category10();
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
 
-d3.csv("charts/cyberindex.csv", function(error, data) {
-  if (error) throw error;
+  var svg = d3.select(css_selector).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  data.forEach(function(d) {
-    d.y = +d.y;
-    d.x = +d.x;
-  });
+  d3.csv(dataset, function(error, data) {
+    if (error) throw error;
 
-  x.domain(d3.extent(data, function(d) { return d.x; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.y; })).nice();
-  
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Internet Penetration Rate (% of Population)");
+    data.forEach(function(d) {
+      d.y = +d.y;
+      d.x = +d.x;
+    });
+    
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Internet Penetration Rate (% of Population)");
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Cyber Security Index")
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Cyber Security Index")
 
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return x(d.x); })
-      .attr("cy", function(d) { return y(d.y); })
-      .style("fill", function(d) { return color(d.z); });
+    svg.selectAll(".dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .style("fill", function(d) { 
+          if(css_selector == "#world"){
+            return color(d.z); 
+          } else {
+            return dotcolor;
+          }
+        });
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 870)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+    legend.append("rect")
+        .attr("x", width - 870)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
 
-  legend.append("text")
-      .attr("x", width -850)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .attr("data-legend-pos", "East Asia and the Pacific", "Europe and Central Asia", "Latin America and the Cabibbean", "Middle East and North Africa", "North America", "South Asia", "Sub-Saharan Africa")
-      .style("text-anchor", "start")
-      .text(function(d) { return d; });
+    legend.append("text")
+        .attr("x", width -850)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d) { return d; });
 
 
-});  
-})
+  });  
+  })
 
+}
+
+
+drawChart('#world', 'charts/cyberindex.csv', "")
+drawChart('#africa', "charts/cyberindex-africa.csv", "red")
+drawChart('#europe', "charts/cyberindex-europe.csv", "#ff9900")
